2c0689768454c8d81234e3a01b0c1cdce878834e
WeiXH <qq212995770@163.com>
2022-11-14 15:10:15 +0800

    增加侯捷cpp代码段

---
+++ build/CMakeFiles/scpp.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/scpp.dir/main.cpp.obj"
+  "libscpp.dll.a"
+  "scpp.exe"
+  "scpp.exe.manifest"
+  "scpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/scpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/workspace/cpp_project/StudyCPlus
+# Build directory: D:/workspace/cpp_project/StudyCPlus/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
